4cc306af0f106857d4dde0bb9a6269c80666d6bd
Daksh Pal <pal.daksh22@gmail.com>
2024-07-20 22:21:50 +0530

    Installed CORS to connect with frontend

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import sendRefer from './controllers/sendRefer.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); 
+app.use(cors({
+    origin: 'http://localhost:5173', // allow requests from this origin
+    credentials: true, // allow credentials (e.g., cookies) to be sent
+  }));
 
 const PORT = process.env.PORT || 3000;
 
